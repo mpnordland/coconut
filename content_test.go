1550d69df2d07f5244ebc21b577ae1edd6c2ac03
Micah Nordland <micah@rehack.me>
2015-01-05 16:50:46 -0500

    added tests for content.go and metadata.go

---
+++ content_test.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+           "testing"
+           "os"
+           "fmt"
+           "time"
+)
+
+const ArticleContent string = `---
+title: Test Article
+author: Coconut Test
+image: 
+tags: 
+ - article 
+ - test 
+ - post 
+date: Jan  5 2015 13:50
+---
+
+This is an article test
+'""<>[]{}&'`
+
+const PageContent string = `---
+title: Test Page
+---
+
+This is a test Page!`
+
+//The article we get is written out in TestMain
+//The tests for Article related functions all
+//get that article and test based upon it
+
+func getArticle(t *testing.T, articleName string) *Article{
+    article, err := GetArticle(articleName)
+    if err != nil {
+        t.Fatal("Existing article not found", err)
+    } 
+    return article
+}
+
+func TestGetArticleWithExistingArticle(t *testing.T) {
+    getArticle(t, "test-article.md")
+}
+
+func TestGetArticleWithMissingArticle(t *testing.T) {
+    article, err := GetArticle("missing-article.md")
+    if err == nil && article != nil {
+        t.Fail()
+    } 
+}
+
+func TestGetArticleMetadata(t *testing.T) {
+    article := getArticle(t, "test-article.md")
+    if article.Title != "Test Article" {
+        t.Fail()
+    }
+    if article.Author != "Coconut Test" {
+        t.Fail()
+    }
+    if article.Tags[0] != "article" || article.Tags[1] != "test" || article.Tags[2] != "post" {
+        t.Fail()
+    }
+    
+    if article.Image != "" && article.HaveImage {
+        t.Fail()
+    }
+
+    time, err := time.Parse(DateFormat, "Jan  5 2015 13:50")
+    if err != nil {
+        t.Fatal("Parsing Date failed, this is a broken test.", err)
+    }
+
+    if !article.Time.Equal(time){
+        t.Fail()
+    }
+}
+
+func TestGetArticleBody(t *testing.T){
+    article := getArticle(t, "test-article.md")
+    if article.Body == ""{
+         t.Fail()
+    }
+}
+
+func TestArticleHasTag(t *testing.T) {
+    article := getArticle(t, "test-article.md")
+    if !article.HasTag("article"){
+        t.Fail()
+    }
+    if !article.HasTag("test") {
+        t.Fail()
+    }
+    if !article.HasTag("post") {
+        t.Fail()
+    }
+}
+
+func TestArticleDate(t *testing.T) {
+    article := getArticle(t, "test-article.md")
+    if article.Date() != "Jan  5 2015 13:50" {
+        t.Fail()
+    }
+}
+
+func TestGetArticles(t *testing.T){
+    articles := GetArticles(func(a *Article) bool {return true})
+    if len(articles) != 2 {
+        t.Fail()
+    }
+}
+
+//Test pages
+
+func getPage(t *testing.T, pageName string) *Page {
+    page, err := GetPage(pageName)
+    if err != nil || page == nil {
+        t.Fatal("Error getting existing page:", err)
+    }
+    return page
+}
+
+func TestGetPageWithExistingPage(t *testing.T) {
+    page, err := GetPage("pages/test-page.md")
+    if err != nil || page == nil {
+        t.Fail()
+    }
+}
+
+func TestGetPageWithMissingPage(t *testing.T) {
+    page, err := GetPage("pages/missing-page.md")
+    if err == nil || page != nil {
+        t.Fail()
+    }
+}
+
+func TestGetPageMetadata(t *testing.T) {
+   page := getPage(t, "pages/test-page.md")
+   if page.Title != "Test Page" {
+       t.Fail()
+   } 
+}
+
+func TestGetPageBody(t *testing.T) {
+   page := getPage(t, "pages/test-page.md")
+   if page.Body == "" {
+       t.Fail()
+    }
+}
+
+func TestMain(m *testing.M){
+    //file names for the tests
+    articleFile := "./articles/test-article.md"
+    pageFile := "./static/pages/test-page.md"
+    
+    //create the files
+    file, err := os.Create(articleFile)
+    if err != nil {
+        fmt.Fprintln(os.Stderr, "Failed setting up for tests", err)
+        os.Exit(1)
+    }
+    file.WriteString(ArticleContent)
+    file.Close()
+
+    file, err = os.Create(pageFile)
+    if err != nil {
+        fmt.Fprintln(os.Stderr, "Failed setting up for tests", err)
+        os.Exit(1)
+    }
+    file.WriteString(PageContent)
+    file.Close()
+
+    //run the tests
+    exitCode := m.Run()
+
+    //clean up after the tests
+    os.Remove(articleFile)
+    os.Remove(pageFile)
+
+    //finally, exit
+    os.Exit(exitCode)
+
+}
---
+++ metadata_test.go
@@ -0,0 +1,50 @@
+package main
+
+import "testing"
+
+//ArticleContent is defined in content_test.go
+//This tests the GetMetadata function and Metadata
+//struct. If this test passes and the content_test.go
+//tests don't the error is probably in content.go or
+//there is a broken test
+
+func TestGetMetadataArticle(t *testing.T) {
+    m, _ := GetMetadata([]byte(ArticleContent))
+    if m.Title != "Test Article" {
+        t.Fail()
+    }        
+    if m.Author != "Coconut Test" {
+        t.Fail()
+    }
+    if m.Date != "Jan  5 2015 13:50" {
+        t.Fail()
+    }
+    if m.Image != "" {
+        t.Fail()
+    }
+
+    if m.Tags[0] != "article" || m.Tags[1] != "test" || m.Tags[2] !="post" {
+        t.Fail()
+    }
+}
+
+func TestGetMetadataPage(t *testing.T) {
+    m, _ := GetMetadata([]byte(PageContent))
+    if m.Title != "Test Page" {
+        t.Fail()
+    }
+
+    if m.Author != "" {
+        t.Fail()
+    }
+    if m.Date != "" {
+        t.Fail()
+    }
+    if m.Image != "" {
+        t.Fail()
+    }
+
+    if len(m.Tags) > 0{
+        t.Fail()
+    }
+}
